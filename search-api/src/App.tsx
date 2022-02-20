29267cb6d6b4051ce1d5d8ddeda583e89d08dcc5
Márcio Gomes <marciogomes@alu.ufc.br>
2022-02-20 20:37:53 -0300

    primeiro commit

---
+++ search-api/src/App.tsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+
+  
+  return (
+    <>
+      <h1> Essa será uma aplicação que usará Api </h1>
+
+    </>
+       
+  );
+}
+
+export default App;
